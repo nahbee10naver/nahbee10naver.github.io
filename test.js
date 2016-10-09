5d5e4b990d8f8227c68c6482b725cb7147eebe35
nahbee <nahbee10@gmail.com>
2016-10-09 18:17:53 +0900

    hihi

--- test.js
+++ test.js
@@ -2,15 +2,18 @@ var last_known_scroll_position = 0;
 var ticking = false;
 
 var ele = document.getElementById('moving');
+var ele2 = document.getElementById('moving2');
 
 function doSomething(scroll_pos) {
   console.log(scroll_pos);
   var hiTop = document.getElementById('hi').getBoundingClientRect().top;
+  var hiTop2 = document.getElementById('hi2').getBoundingClientRect().top;
   var hei = window.innerHeight;
   var wid = window.innerWidth;
-  var perc = hiTop/hei;
+  var perc = (hiTop/hei)-0.2;
+  var perc2 = (hiTop2/hei)-0.2;
   ele.style.left = wid*perc;
-  console.log(perc);
+  ele2.style.left = wid*perc2;
 }
 
 window.addEventListener('scroll', function(e) {
