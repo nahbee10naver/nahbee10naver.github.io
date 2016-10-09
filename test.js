3ee8cb23e0a18e81e9f658d6be027d6b24c838e8
nahbee <nahbee10@gmail.com>
2016-10-10 00:38:29 +0900

    scroll

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ var ticking = false;
 
 var ele_arr = document.getElementsByClassName('thing');
 var hi_arr = document.getElementsByClassName('hi');
-var for_fix = document.getElementById('letsfix');
+var gu = document.getElementById('gu');
+var dong = document.getElementById('dong');
+
 var perc = [];
 var top_val = [];
 var hei = window.innerHeight;
@@ -13,30 +15,34 @@ for (var i = 0; i <ele_arr.length; i++) {
 	top_val[i] = 50+Math.random()*17;
 };
 
+var wor_list_l = ["소개","방법"];
+var wor_list_r = ["소개","방법"];
+var div_arr  = document.getElementsByClassName('wrapper');
+
 function doSomething(scroll_pos) {
-  console.log(scroll_pos);
-  /*var hiTop = document.getElementById('hi').getBoundingClientRect().top;
-  var hiTop2 = document.getElementById('hi2').getBoundingClientRect().top;*/
   for (var i = 0; i <hi_arr.length; i++) {
-  	console.log(hi_arr[i].getBoundingClientRect().top);
 	perc[i] = hi_arr[i].getBoundingClientRect().top/hei-0.3;
-	console.log(perc[i]);
 
   };
   for (var i = 0; i <ele_arr.length; i++) {
   	if(perc[i]<=0.5){
   		ele_arr[i].style.display = "inherit";
   		ele_arr[i].style.top = top_val[i] + '%';
-  		console.log(ele_arr[i].style.top);
   		ele_arr[i].style.left = wid*perc[i];
   	}else{
   		ele_arr[i].style.display = "none";
   	}
 		
   };
-  for_fix.style.position="fixed";
-  for_fix.style.top="100px";
-  for_fix.style.left="20px";
+  for (var i = 0; i <div_arr.length; i++) {
+	var div_perc = div_arr[i].getBoundingClientRect().top/hei;
+	if(div_perc<0.8){
+		gu.innerHTML=wor_list_l[i];
+		dong.innerHTML=wor_list_r[i];
+	}
+
+  };
+
 }
 
 window.addEventListener('scroll', function(e) {
