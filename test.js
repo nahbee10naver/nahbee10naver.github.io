d8a422c2e51324ccf3451e3e3b6892d40bbbd34f
nahbee <nahbee10@gmail.com>
2016-10-10 19:48:27 +0900

    text size

--- test.js
+++ test.js
@@ -19,16 +19,35 @@ var wor_list_l = ["소개","방법"];
 var wor_list_r = ["소개","방법"];
 var div_arr  = document.getElementsByClassName('wrapper');
 
+
+var path = document.querySelector('#nahee path');
+console.log(path);
+var length = path.getTotalLength();
+// Clear any previous transition
+path.style.transition = path.style.WebkitTransition =
+  'none';
+// Set up the starting positions
+path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDashoffset = length;
+// Trigger a layout so styles are calculated & the browser
+// picks up the starting position before animating
+path.getBoundingClientRect();
+
 function doSomething(scroll_pos) {
   for (var i = 0; i <hi_arr.length; i++) {
-	perc[i] = hi_arr[i].getBoundingClientRect().top/hei-0.3;
-
+	perc[i] = hi_arr[i].getBoundingClientRect().top/hei-0.2;
+	path.style.transition = path.style.WebkitTransition =
+	  'stroke-dashoffset 1s ease-in-out';
+	// Go!
+	path.style.strokeDashoffset = length*(1-(i/hi_arr.length));
   };
   for (var i = 0; i <ele_arr.length; i++) {
   	if(perc[i]<=0.5){
   		ele_arr[i].style.display = "inherit";
   		ele_arr[i].style.top = top_val[i] + '%';
   		ele_arr[i].style.left = wid*perc[i];
+		gu.innerHTML = hi_arr[i].dataset.gu;
+		dong.innerHTML = hi_arr[i].dataset.dong;
   	}else{
   		ele_arr[i].style.display = "none";
   	}
@@ -36,7 +55,7 @@ function doSomething(scroll_pos) {
   };
   for (var i = 0; i <div_arr.length; i++) {
 	var div_perc = div_arr[i].getBoundingClientRect().top/hei;
-	if(div_perc<0.8){
+	if(div_perc<0.8 && div_perc >=0){
 		gu.innerHTML=wor_list_l[i];
 		dong.innerHTML=wor_list_r[i];
 	}
@@ -55,3 +74,4 @@ window.addEventListener('scroll', function(e) {
   }
   ticking = true;
 });
+
