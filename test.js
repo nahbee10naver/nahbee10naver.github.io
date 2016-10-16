7713874fc2313c0982cd927967f3d88150e42c99
nahbee <nahbee10@gmail.com>
2016-10-16 20:28:27 +0900

    rotation

--- test.js
+++ test.js
@@ -52,16 +52,8 @@ path.getBoundingClientRect();
 
 function doSomething(scroll_pos) {
 
-  //for (var i = 0; i < bgimgs.length; i++) {
-  	//$('#bg_nh_1').css('transform','rotate(' + scroll_pos/5 + Math.random(10,40)+ 'deg)');
   	document.getElementById('bg_nh_1').style.webkitTransform = 'rotate(' + scroll_pos/15 + Math.random(10,40)+ 'deg)';
   	document.getElementById('bg_nh_2').style.webkitTransform = 'rotate(' + scroll_pos/15 + Math.random(10,40)+ 'deg)';
-  	/*$('#bg_nh_1').css('-webkit-transform-2d','rotate(' + scroll_pos/5 + Math.random(10,40)+ 'deg)');
-  	$('#bg_nh_1').css('-moz-transform','rotate(' + scroll_pos/5 + Math.random(10,40)+ 'deg)');
-  	//$('#bg_nh_2').css('transform','rotate(' + scroll_pos/5 + Math.random(1,10)+ 'deg)');
-  	$('#bg_nh_2').css('-webkit-transform-2d','rotate(' + scroll_pos/5 + Math.random(1,10)+ 'deg)');
-  	$('#bg_nh_2').css('-moz-transform','rotate(' + scroll_pos/5 + Math.random(1,10)+ 'deg)');*/
-  //};
 
   for (var i = 0; i <hi_arr.length; i++) {
 	perc[i] = hi_arr[i].getBoundingClientRect().top/hei-0.2;
