6f1e53f5a3364e8db58610158fda81aa73c1ec90
nahbee <nahbee10@gmail.com>
2016-11-06 23:29:09 +0900

    ad added

--- test.js
+++ test.js
@@ -259,4 +259,22 @@ function buttonFunc(btn){
 
 }
 
+$(document).ready(function() {
+	var offset = 800;
+	var duration = 300;
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$('.back-to-top').fadeIn(duration);
+		} else {
+ 			$('.back-to-top').fadeOut(duration);
+		}
+	});
+ 
+	$('.back-to-top').click(function(event) {
+		event.preventDefault();
+ 		$('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	})
+});
+
 
