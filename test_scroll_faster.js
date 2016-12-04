548a15a904417701cfaf87c30c5dec7383002baf
nahbee <nahbee10@gmail.com>
2016-12-04 18:59:43 +0900

    favi

--- test_scroll_faster.js
+++ test_scroll_faster.js
@@ -58,111 +58,6 @@ path.style.strokeDashoffset = length;
 // picks up the starting position before animating
 path.getBoundingClientRect();
 
-/*function doSomething(scroll_pos) {
-
-  for (var i = 0; i <hi_arr.length; i++) {
-	perc[i] = hi_arr[i].getBoundingClientRect().top/hei-0.2;
-	
-  };
-  for (var i = 0; i <ele_arr.length; i++) {
-  	if(perc[i]<=0.5){
-  		ele_arr[i].style.display = "inherit";
-  		ele_arr[i].style.top = top_val[i] + '%';
-  		ele_arr[i].style.left = wid*perc[i];
-		gu.innerHTML = hi_arr[i].dataset.gu;
-		dong.innerHTML = hi_arr[i].dataset.dong;
-		path.style.transition = path.style.WebkitTransition =
-		  'stroke-dashoffset 1s ease-in-out';
-		// Go!
-		var appliedPerc = 0;
-		var i_num;
-		switch (hi_arr[i].dataset.gu) {
-		  case "종로구":
-		    appliedPerc = 1 - trail_perc[0];
-		    i_num = 1;
-		    ani_arr[0].setAttribute('dur','500ms');
-    		ani_arr[0].setAttribute('repeatCount','indefinite');
-		    ani_arr[0].beginElement();
-		    //first_bg.style.display = 'none';
-		    break;
-		  case "마포구":
-		    appliedPerc = 1 - trail_perc[1];
-		    i_num = 2;
-		    //ani_arr[1].setAttribute('begin','0.1s');
-		    break;
-		  case "영등포구":
-		    appliedPerc = 1 - trail_perc[2];
-		    i_num = 3;
-		    //ani_arr[1].endElement();
-		    break;
-		  case "동작구":
-		    appliedPerc = 1 - trail_perc[3];
-		    i_num = 4;
-		    break;
-		  case "관악구":
-		    appliedPerc = 1 - trail_perc[4];
-		    i_num = 5;
-		    //ani_arr[3].endElement();
-		    break;
-	  	  case "서초구":
-		    appliedPerc = 1 - trail_perc[5];
-		    i_num = 6;
-		    //ani_arr[4].endElement();
-		    break;
-		  case "강남구":
-		    appliedPerc = 1 - trail_perc[6];
-		    i_num = 7;
-		    //ani_arr[4].endElement();
-		    break;
-		  case "송파구":
-		    appliedPerc = 1 - trail_perc[7];
-		    i_num = 8;
-		    //ani_arr[5].endElement();
-		    break;
-		  case "강동구":
-		    appliedPerc = 1 - trail_perc[8];
-		    i_num = 9;
-		    //ani_arr[6].endElement();
-		    break;
-		}
-		path.style.strokeDashoffset = length*appliedPerc;
-
-		for (var t = 0; t <i_num; t++) {
-			document.getElementById(gu_name[t]).style.display="inherit";
-		};
-		if(i_num<9){
-			for (var p = i_num; p<9; p++) {
-				document.getElementById(gu_name[p]).style.display="none";
-			};
-		}
-
-		if(i_num>1){
-			ani_mani(i_num);
-		}
-		
-  	}else{
-  		ele_arr[i].style.display = "none";
-  	}
-		
-  };
-  for (var i = 0; i <div_arr.length; i++) {
-	var div_perc = div_arr[i].getBoundingClientRect().top/hei;
-	if(div_perc<0.8 && div_perc >=0){
-		gu.innerHTML=wor_list_l[i];
-		dong.innerHTML=wor_list_r[i];
-	}
-
-  };
-  if(scroll_pos > (document.body.scrollHeight)*0.9){
-  	ani_arr[8].endElement();
-  }else if(scroll_pos < (document.body.scrollHeight)*0.02){
-  	for (var p = 0; p<9; p++) {
-		document.getElementById(gu_name[p]).style.display="none";
-	};
-  }
-
-}*/
-
 function ani_mani(n){
 	ani_arr[n-2].setAttribute('dur','0');
     ani_arr[n-2].setAttribute('repeatCount','0');
@@ -370,5 +265,11 @@ $(document).ready(function() {
 		return false;
 	})
 });
+
+
+var mq = window.matchMedia( "(max-width: 600px)" );
+if (mq.matches) {
+	document.getElementById("bg_img_as_div").src="img/bg_m_l.png";
+}
 
 
